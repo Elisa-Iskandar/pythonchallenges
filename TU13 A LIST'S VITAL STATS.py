0e166a859c0ffe71812ba38489f50aebc7b2eb78
Elisa <58130797+Elisa-Iskandar@users.noreply.github.com>
2023-10-09 13:43:47 +0800

    Create TU13 A LIST'S VITAL STATS.py

---
+++ TU13 A LIST'S VITAL STATS.py
@@ -0,0 +1,8 @@
+#Exercise 1: Create a list of number and total it up.
+#EXERCISE 2: Create a list's vital statistics including total, minimum, maxiumum, length & mean average
+mylist = [0,1,2,3]
+print(sum(mylist))
+print(min(mylist))
+print(max(mylist))
+print(len(mylist))
+print(sum(mylist)/len(mylist))
