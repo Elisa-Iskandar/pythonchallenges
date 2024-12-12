7289bb154c08483267c8f9ada7e1c0e40be4597d
Elisa <58130797+Elisa-Iskandar@users.noreply.github.com>
2024-12-12 08:17:35 +0800

    Create GC26 DRUNKOMETER.py

---
+++ GC26 DRUNKOMETER.py
@@ -0,0 +1,10 @@
+print("pazazz pazazzes tazzas zaqazip pizzazzes pizzazz bezazzes azazel zaqqum gozzan bezazz mazzard hazzan pizzazz pizazzes razzed piazzas razzes pappox pizzas ezzard poppa cozzes cazzie razzer")
+first = input("Type the phrase above: ")
+if first == "pazazz pazazzes tazzas zaqazip pizzazzes pizzazz bezazzes azazel zaqqum gozzan bezazz mazzard hazzan pizzazz pizazzes razzed piazzas razzes pappox pizzas ezzard poppa cozzes cazzie razzer":
+    second = input("Type it again: ")
+    if second == "pazazz pazazzes tazzas zaqazip pizzazzes pizzazz bezazzes azazel zaqqum gozzan bezazz mazzard hazzan pizzazz pizazzes razzed piazzas razzes pappox pizzas ezzard poppa cozzes cazzie razzer":
+        print("Not drunk!")
+    else:
+        print("Incompetent!")
+else:
+    print("Incompetent!")
